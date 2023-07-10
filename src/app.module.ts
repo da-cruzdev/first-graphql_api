04b88803fcc413632ec60ecf2e7dc4f30244790d
Da-cruz <wilfrieddacruz@arolitec.com>
2023-07-10 09:57:39 +0000

    create auth resource for user authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { TodoModule } from './todo/todo.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { PrismaService } from './prisma/prisma.service';
       // sortSchema: true,
     }),
     TodoModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
